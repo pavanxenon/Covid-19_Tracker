56673b4ff4a7dc8db215a4357722a14ec5c912fe
pavanxenon <pavan.kumar.uspd@gmail.com>
2021-05-22 19:22:39 +0530

    covid-19 tracker done

---
+++ app/src/main/java/com/example/covid_19tracker/User.java
@@ -0,0 +1,15 @@
+package com.example.covid_19tracker;
+
+public class User {
+
+    public String fullName, age, email;
+
+    public User(){
+
+    }
+    public User(String fullName, String age, String email) {
+        this.fullName = fullName;
+        this.age = age;
+        this.email = email;
+    }
+}
